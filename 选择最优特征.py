4bbd64909b0c1de9917542b96a6fd8ac8dd36346
dingChangmao <1255535360@qq.com>
2019-04-25 05:02:00 -0400

    练习

--- 选择最优特征.py
+++ 选择最优特征.py
@@ -74,6 +74,7 @@ Modify:
     2017-03-30
 """
 def splitDataSet(dataSet, axis, value):
+    # print(axis)
     retDataSet = []                                        #创建返回的数据集列表
     for featVec in dataSet:                             #遍历数据集
         if featVec[axis] == value:
@@ -96,20 +97,33 @@ Modify:
 """
 def chooseBestFeatureToSplit(dataSet):
     numFeatures = len(dataSet[0]) - 1                    #特征数量
+
     baseEntropy = calcShannonEnt(dataSet)                 #计算数据集的香农熵
+
     bestInfoGain = 0.0                                  #信息增益
     bestFeature = -1                                    #最优特征的索引值
     for i in range(numFeatures):                         #遍历所有特征
         #获取dataSet的第i个所有特征
+
         featList = [example[i] for example in dataSet]
+        # print(featList)
+
         uniqueVals = set(featList)                         #创建set集合{},元素不可重复
+
         newEntropy = 0.0                                  #经验条件熵
+
         for value in uniqueVals:                         #计算信息增益
+
             subDataSet = splitDataSet(dataSet, i, value)         #subDataSet划分后的子集
+
             prob = len(subDataSet) / float(len(dataSet))           #计算子集的概率
+            # print(prob)
             newEntropy += prob * calcShannonEnt(subDataSet)     #根据公式计算经验条件熵
+            print(newEntropy)
+
         infoGain = baseEntropy - newEntropy                     #信息增益
-        print("第%d个特征的增益为%.3f" % (i, infoGain))            #打印每个特征的信息增益
+
+        # print("第%d个特征的增益为%.3f" % (i, infoGain))            #打印每个特征的信息增益
         if (infoGain > bestInfoGain):                             #计算信息增益
             bestInfoGain = infoGain                             #更新信息增益，找到最大的信息增益
             bestFeature = i                                     #记录信息增益最大的特征的索引值
